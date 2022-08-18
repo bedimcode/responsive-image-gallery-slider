f1c888c15c2ff36b7ac7e0c57e91df61e0e0762b
Marlon <61248995+bedimcode@users.noreply.github.com>
2022-08-18 10:41:34 -0500

    Add files via upload

---
+++ assets/js/main.js
@@ -0,0 +1,26 @@
+/*=============== SWIPER JS GALLERY ===============*/
+let swiperCards = new Swiper(".gallery-cards", {
+  loop: true,
+  loopedSlides: 5,
+  cssMode: true,
+  effect: 'fade',
+});
+  
+let swiperThumbs = new Swiper(".gallery-thumbs", {
+  loop: true,
+  loopedSlides: 5,
+  slidesPerView: 3,
+  centeredSlides: true,
+  slideToClickedSlide: true,
+
+  pagination: {
+    el: ".swiper-pagination",
+    type: "fraction",
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
+
+swiperThumbs.controller.control = swiperCards;
